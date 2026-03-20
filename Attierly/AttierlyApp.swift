5c435c7b8345902f33aafeb7bfd744617b4d9e20
giuliobarde <giuliobarde@gmail.com>
2026-03-20 11:46:07 -0400

    Initial commit

---
+++ Attierly/AttierlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttierlyApp.swift
+//  Attierly
+//
+//  Created by Giulio Bardelli on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct AttierlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
